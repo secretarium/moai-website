c055868215232f2864224ef392284ac55f0bbf53
Florian Guitton <florian@secretarium.com>
2020-10-09 13:10:54 +0100

    chore: Fixing Tailwind class apply on Markdown

--- packages/www/lib/markdownToHtml.ts
+++ packages/www/lib/markdownToHtml.ts
@@ -1,20 +1,9 @@
-import remark from 'remark'
-import html from 'remark-html'
-import all from 'mdast-util-to-hast/lib/all';
+import remark from 'remark';
+import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
     const result = await remark().use(html, {
-        sanitize: true,
-        handlers: {
-            heading: (h: any, node: any) => {
-                const augmentedNode = {
-                    ...node
-                };
-                return h(augmentedNode, 'h' + augmentedNode.depth, {
-                    className: ['text-3xl']
-                }, all(h, augmentedNode))
-            }
-        }
+        sanitize: true
     }).process(markdown)
     return result.toString()
 }
