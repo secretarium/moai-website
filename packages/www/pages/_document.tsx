6f13a77cfae5214933e1ae91016e9d4f81f322af
Florian Guitton <florian@secretarium.com>
2020-12-17 22:01:31 +0100

    fix: Fixing no-document-viewport-meta issue

--- packages/www/pages/_document.tsx
+++ packages/www/pages/_document.tsx
@@ -5,9 +5,7 @@ export default class AppDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head>
-                    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
-                </Head>
+                <Head />
                 <body className="overflow-x-hidden" style={{
                     color: '#213963'
                 }}>
